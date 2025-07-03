92f225922957696da31068f209bd87b435514a92
daqh <valeriodipasq@gmail.com>
2025-07-03 13:41:03 +0200

    Update

--- seedset.py
+++ seedset.py
@@ -11,7 +11,7 @@ def random_seeds(G: nx.Graph, c: callable) -> set[int]:
         while True:
             # Add a random node to S_p
             S_p.add(randint(0, G.number_of_nodes() - 1))
-            if sum([c(n, G) for n in S_p]) >= k:
+            if sum([c(n, G) for n in S_p]) > k:
                 break
             S_d = S_p
         # Now S_d is a set of nodes that can be selected
@@ -60,27 +60,33 @@ def wtss(G: nx.Graph, c: callable, t: np.ndarray):
         return S, sum([c(n, G) for n in S])
     return wrapper
 
-def purp(G: nx.Graph, c: callable):
+from math import ceil, log2
+
+def purp(G: nx.Graph, c: callable, d: int):
     V = set(G.nodes())
     def wrapper(k: int):
         i = 0
         S = set()
         ss_cost = 0
+        _G = nx.Graph(nx.subgraph(G, V)) # Induced subgraph
         while True:
-            _G = nx.subgraph(G, V.difference(S)) # Induced subgraph
             i += 1
-            print(f"Iteration {i}, remaining nodes: {_G.number_of_nodes()}", end='\r')
-            betweenness = nx.betweenness_centrality(_G) # Calculate betweenness centrality
-            betweenness = np.array([betweenness[v] if v in betweenness else 0 for v in range(max(G.nodes()) + 1)])
+            print(f"Iteration {i}, S: {len(S)} k: {k}")
+            betweenness = nx.betweenness_centrality(_G, ceil(log2(G.number_of_nodes()))) # Calculate betweenness centrality
+            betweenness = np.array([betweenness[v] if v in betweenness else -1 for v in range(max(G.nodes()) + 1)])
             # Get the node with maximum betweenness centrality
-            u = np.argmax(betweenness)
-            # Check the cost
-            u_cost = c(u, G)
-            if ss_cost + u_cost > k:
+            candidates = np.argsort(-betweenness)[:d]
+            for u in candidates:
+                # Check the cost
+                u_cost = c(u, G)
+                if ss_cost + u_cost > k:
+                    _G.remove_node(u)
+                    continue
+                S.add(u)
+                ss_cost += u_cost
+                # Remove the node from the graph
+                _G.remove_node(u)
+            if ss_cost >= k or len(_G.nodes()) == 0:
                 break
-            S.add(u)
-            ss_cost += u_cost
-            # Remove the node from the graph
-            _G.remove_node(u)
         return S, ss_cost
     return wrapper
